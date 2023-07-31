59e6929fb5648a77c23d982b3748a3aa31ba1ed9
ogotikbrah <kengaraogotiabro@gmail.com>
2023-07-31 04:22:33 -0700

    update

--- 0x07-python-test_driven_development/6-max_integer_test.py
+++ 0x07-python-test_driven_development/6-max_integer_test.py
@@ -1,43 +1,18 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([...])"""
+"""Module to find the max integer in a list
+"""
 
-import unittest
-max_integer = __import__('6-max_integer').max_integer
 
-"""we create a test case by accessing Testcase method"""
-class TestMaxInteger(unittest.TestCase):
-    def test_first_element(self):
-        """Test the first one"""
-        first_element = [40, 30, 20, 10]
-        self.assertEqual(max_integer(first), 40)
-
-    def test_last_max(self):
-        """Test the last one"""
-        last_max = [10, 10, 90, 100]
-        self.assertEqual(max_integer(last), 100)
-
-    def test_empty(self):
-        """Test for an empty list passed"""
-        empty = []
-        self.assertEqual(max_integer(empty), None)
-
-    def test_single(self):
-        """looks for single element in the lis"""
-        single = [2]
-        self.assertEqual(max_integer(single), 2)
-    def test_negative(self):
-        """test for negative in list"""
-        negative = [-4, -199, -23, -1]
-        self.assertEqual(max_integer(negative), -1)
-    def test_strings(self):
-        """test when given a string"""
-        strings = ["what", "happened"]
-        self.assertEqual(max_integer(strings), 's')
-
-    def test_floats(self):
-        """tests for floats"""
-        floats = [3, 7.8, 9.8 3, 5]
-        self.assertEqual(max_integer(floats), 9.8)
-
-if __name__ == '__main__':
-    unittest.main()
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
